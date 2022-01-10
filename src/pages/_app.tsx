ec40dacda09d0650d6ae7709c5a887bea79f8b04
Gonzalo Flores <gonzalo.flores@estoes.me>
2022-01-10 14:47:53 -0300

    add favico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,43 @@ export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Gonzalo Flores</title>
+        <meta name="theme-color" content={theme.palette.primary.main} />
+
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="*"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        ></link>
+
+        {/* FAVICON */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
